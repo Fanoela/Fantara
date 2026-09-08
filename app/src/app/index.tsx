9ed2b36b94c3f17da01b51b5b04f99951f6faa13
Fanoela <fanoela011@gmail.com>
2026-09-08 15:30:19 +0300

    add buttons

--- app/src/app/index.tsx
+++ app/src/app/index.tsx
@@ -1,98 +1,72 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
+import { Text, View, StyleSheet, ImageBackground, Pressable } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+    <ImageBackground
+      source={require('@/assets/images/bg.jpeg')}
+      style={styles.container}
+      resizeMode="cover"
+    >
+      <Text style={styles.title}>HAIKINTANA</Text>
+      <Text style={styles.subtitle}>Ho an'ny rehetra</Text>
+       <Pressable
+        style={styles.button}
+        onPress={() => alert('Button pressed!')}
+      >
+        <Text style={styles.buttonText}>Lesona</Text>
+      </Pressable>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+             <Pressable
+        style={styles.button}
+        onPress={() => alert('Button pressed!')}
+      >
+        <Text style={styles.buttonText}>Fanontaniana</Text>
+      </Pressable>
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert('Button pressed!')}
+      >
+        <Text style={styles.buttonText}>Hanova</Text>
+      </Pressable>
 
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+            <Pressable
+        style={styles.button}
+        onPress={() => alert('Button pressed!')}
+      >
+        <Text style={styles.buttonText}>Hivoaka</Text>
+      </Pressable>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
   },
   title: {
-    textAlign: 'center',
+    color: '#FFFFFF',
+    fontSize: 37,
+    fontWeight: 'bold',
   },
-  code: {
-    textTransform: 'uppercase',
+  subtitle: {
+    color: '#FFFFFF',
+    fontSize: 24,
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+
+  button: {
+    backgroundColor: '#ff9500',
+    paddingVertical: 14,
+    paddingHorizontal: 32,
+    borderRadius: 20,
+    marginTop: 20,
+    width:180,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: '700',
   },
 });
